8c9df00ed166fd7ac140ad715c77bca0712e9aba
MMaestrelliLeobons <mml@globo.com>
2016-03-06 16:40:24 -0300

    Added opportunity view. Fixed card word-break.

--- public/app/components/opportunity/controllers/opportunityCtrl.js
+++ public/app/components/opportunity/controllers/opportunityCtrl.js
@@ -1,4 +1,9 @@
 angular.module('baseapolo').controller('opportunityCtrl', function ($scope, newsAPI, newsId) {
     $scope.news = newsAPI.getNewsById(newsId);
+    if ($scope.news) {
+        if ($scope.news.stars) {
+            $scope.emails = $scope.news.stars;
+        }
+    } 
     console.log("opportunityCtrl");
 });
